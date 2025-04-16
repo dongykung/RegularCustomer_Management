928948b03a738afcda51fe6a28f6e98ec7c05e7f
kim dong kyung <kdkd422@naver.com>
2025-04-17 01:19:45 +0900

    Feat - 고객생성 기본정보 모델

---
+++ RegularCustomerManagement/app/src/main/java/com/dkproject/regularcustomermanagement/presentation/model/BasicInfo.kt
@@ -0,0 +1,8 @@
+package com.dkproject.regularcustomermanagement.presentation.model
+
+data class BasicInfo (
+    val name: String = "",
+    val phoneNumber: String? = null,
+    val nickName: String? = null,
+    val carNumber: String? = null
+)
