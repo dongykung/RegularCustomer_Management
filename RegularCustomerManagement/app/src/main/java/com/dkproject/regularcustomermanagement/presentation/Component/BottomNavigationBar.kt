7cd8f5d668c5269e0083b174d54b4bf3e7f9466b
kim dong kyung <kdkd422@naver.com>
2025-04-17 01:19:20 +0900

    Feat - 네비게이션 바 탭 구조에서만 나오게 처리

--- RegularCustomerManagement/app/src/main/java/com/dkproject/regularcustomermanagement/presentation/Component/BottomNavigationBar.kt
+++ RegularCustomerManagement/app/src/main/java/com/dkproject/regularcustomermanagement/presentation/Component/BottomNavigationBar.kt
@@ -1,6 +1,7 @@
 package com.dkproject.regularcustomermanagement.presentation.Component
 
 import android.util.Log
+import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.NavigationBar
@@ -12,37 +13,52 @@ import androidx.compose.runtime.getValue
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.toRoute
 import com.dkproject.regularcustomermanagement.presentation.navigation.TabScreen
+import com.dkproject.regularcustomermanagement.presentation.navigation.TabScreenList.tabNavItem
 
 @Composable
 fun BottomNavigationBar(
     navController: NavHostController,
+    currentRoute : String? = null
 ) {
-    val navItem = listOf(
-        TabScreen.Home,
-        TabScreen.Customer,
-        TabScreen.Handover
-    )
-    val currentBackStackEntry by navController.currentBackStackEntryAsState()
-    NavigationBar {
-        navItem.forEach { item ->
-            NavigationBarItem(
-                selected = item.route == currentBackStackEntry?.destination?.route,
-                onClick = {
-                    navController.navigate(item.route) {
-                        popUpTo(navController.graph.id) {
-                            saveState = true
+    val navItem = tabNavItem
+    AnimatedVisibility(navItem.any { it.route == currentRoute }) {
+        NavigationBar(tonalElevation = 0.dp) {
+            navItem.forEach { item ->
+                NavigationBarItem(
+                    selected = item.route == currentRoute,
+                    onClick = {
+                        navController.navigate(item.route) {
+                            popUpTo(navController.graph.id) {
+                                saveState = true
+                            }
+                            launchSingleTop = true
+                            restoreState = true
                         }
-                        launchSingleTop = true
-                        restoreState = true
-                    }
-                },
-                icon = { Icon(imageVector = item.icon, contentDescription = stringResource(item.description)) },
-                label = { Text(text = stringResource(item.title), fontWeight = FontWeight.Bold) },
-            )
+                    },
+                    icon = {
+                        Icon(
+                            imageVector = item.icon,
+                            contentDescription = stringResource(item.description)
+                        )
+                    },
+                    label = {
+                        Text(
+                            text = stringResource(item.title),
+                            fontWeight = FontWeight.Bold
+                        )
+                    },
+                    colors = NavigationBarItemDefaults.colors(
+                        indicatorColor = Color.Transparent,
+                        selectedIconColor = MaterialTheme.colorScheme.primary,
+                        selectedTextColor = MaterialTheme.colorScheme.primary
+                    )
+                )
+            }
         }
     }
 }
