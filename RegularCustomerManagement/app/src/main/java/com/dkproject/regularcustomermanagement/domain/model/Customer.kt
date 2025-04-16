0f8c6d59c0b44204417e4830147a44893b9ffa06
kim dong kyung <kdkd422@naver.com>
2025-04-17 01:17:36 +0900

    Feat - Customer 모델 초기값 세팅

--- RegularCustomerManagement/app/src/main/java/com/dkproject/regularcustomermanagement/domain/model/Customer.kt
+++ RegularCustomerManagement/app/src/main/java/com/dkproject/regularcustomermanagement/domain/model/Customer.kt
@@ -3,15 +3,15 @@ package com.dkproject.regularcustomermanagement.domain.model
 import java.util.Date
 
 data class Customer(
-    val id: String, //고객 아이디
-    val name: String, // 고객 이름
-    val carNumber: String?, // 고객 차량 번호
-    val affiliation: String?, // 고객 소속
-    val visitedList: List<Date>, // 고객 방문 리스트
-    val phoneNumber: String?, // 고객 휴대폰 번호
-    val memoList: List<String>, // 고객 메모 리스트
-    val isStar: Boolean, // 고객 즐겨찾기 여부
-    val nickName: String?, // 고객 닉네임
-    val createdAt: Date, // 고객 생성 날짜
-    val tags: List<String> // 고객 태그 리스트
+    val id: String = "", //고객 아이디
+    val name: String = "", // 고객 이름
+    val carNumber: String? = null, // 고객 차량 번호
+    val affiliation: String? = null, // 고객 소속
+    val visitedList: List<Date> = emptyList(), // 고객 방문 리스트
+    val phoneNumber: String? = null, // 고객 휴대폰 번호
+    val memoList: List<String> = emptyList(), // 고객 메모 리스트
+    val isStar: Boolean = false, // 고객 즐겨찾기 여부
+    val nickName: String? = null, // 고객 닉네임
+    val createdAt: Date = Date(), // 고객 생성 날짜
+    val tags: List<String> = emptyList() // 고객 태그 리스트
 )
