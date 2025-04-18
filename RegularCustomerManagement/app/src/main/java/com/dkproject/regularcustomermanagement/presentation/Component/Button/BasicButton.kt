ecb330239a693a0d23646c2b835d7b764d208bb6
kim dong kyung <kdkd422@naver.com>
2025-04-18 14:39:12 +0900

    Feat - 버튼 컴포넌트 loading 파라미터 추가, loading 시 프로그래스 바 추가

--- RegularCustomerManagement/app/src/main/java/com/dkproject/regularcustomermanagement/presentation/Component/Button/BasicButton.kt
+++ RegularCustomerManagement/app/src/main/java/com/dkproject/regularcustomermanagement/presentation/Component/Button/BasicButton.kt
@@ -1,9 +1,12 @@
 package com.dkproject.regularcustomermanagement.presentation.Component.Button
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Button
+import androidx.compose.material3.CircularProgressIndicator
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.res.dimensionResource
@@ -14,6 +17,7 @@ fun BasicButton(
     title: String,
     onClick: () -> Unit,
     modifier: Modifier = Modifier,
+    loading: Boolean = false,
     shape: Shape = RoundedCornerShape(dimensionResource(R.dimen.padding_small)),
     enabled: Boolean = true,
 ) {
@@ -23,6 +27,9 @@ fun BasicButton(
         shape = shape,
         modifier = modifier
     ) {
-        Text(text = title)
+        Row(verticalAlignment = Alignment.CenterVertically) {
+            Text(text = title)
+            if(loading) CircularProgressIndicator()
+        }
     }
 }
