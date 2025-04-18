b08ce4f965d1f4a4f5e6071d55f74dada3efab8d
kim dong kyung <kdkd422@naver.com>
2025-04-18 09:23:13 +0900

    Feat - Customer DataSource

---
+++ RegularCustomerManagement/app/src/main/java/com/dkproject/regularcustomermanagement/data/datasource/CustomerDataSource.kt
@@ -0,0 +1,34 @@
+package com.dkproject.regularcustomermanagement.data.datasource
+
+import com.dkproject.regularcustomermanagement.data.dao.CustomerDao
+import com.dkproject.regularcustomermanagement.data.model.CustomerEntity
+import kotlinx.coroutines.flow.Flow
+import javax.inject.Inject
+
+interface CustomerDataSource {
+    fun getAllCustomers(): Flow<List<CustomerEntity>>
+    suspend fun addCustomer(customerEntity: CustomerEntity)
+    suspend fun updateCustomer(customerEntity: CustomerEntity)
+    suspend fun deleteCustomer(customerEntity: CustomerEntity)
+}
+
+class CustomerDataSourceImpl @Inject constructor(
+    private val customerDao: CustomerDao
+): CustomerDataSource {
+    override fun getAllCustomers(): Flow<List<CustomerEntity>> {
+        return customerDao.getAllCustomers()
+    }
+
+    override suspend fun addCustomer(customerEntity: CustomerEntity) {
+        customerDao.insertCustomer(customerEntity)
+    }
+
+    override suspend fun updateCustomer(customerEntity: CustomerEntity) {
+        customerDao.updateCustomer(customerEntity)
+    }
+
+    override suspend fun deleteCustomer(customerEntity: CustomerEntity) {
+        customerDao.deleteCustomer(customerEntity)
+    }
+
+}
