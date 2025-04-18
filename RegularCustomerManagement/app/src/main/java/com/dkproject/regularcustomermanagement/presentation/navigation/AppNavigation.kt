047c7a7323e23252662bb038e9c7ce53ea782bec
kim dong kyung <kdkd422@naver.com>
2025-04-18 20:42:43 +0900

    Feat - 네비게이션 고객스크린 연결

--- RegularCustomerManagement/app/src/main/java/com/dkproject/regularcustomermanagement/presentation/navigation/AppNavigation.kt
+++ RegularCustomerManagement/app/src/main/java/com/dkproject/regularcustomermanagement/presentation/navigation/AppNavigation.kt
@@ -20,6 +20,7 @@ import com.dkproject.regularcustomermanagement.presentation.model.BaseUiEvent
 import com.dkproject.regularcustomermanagement.presentation.ui.createCustomer.CreateCustomerScreen
 import com.dkproject.regularcustomermanagement.presentation.ui.createCustomer.CreateCustomerViewModel
 import com.dkproject.regularcustomermanagement.presentation.ui.customer.CustomerScreen
+import com.dkproject.regularcustomermanagement.presentation.ui.customer.CustomerViewModel
 
 @Composable
 fun AppNavigation(
@@ -34,7 +35,15 @@ fun AppNavigation(
         }
 
         composable(TabScreen.Customer.route) {
-            CustomerScreen(modifier = modifier)
+            val viewModel: CustomerViewModel = hiltViewModel()
+            val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+            val searchQuery by viewModel.searchQuery.collectAsStateWithLifecycle()
+            val searchResults by viewModel.searchResults.collectAsStateWithLifecycle()
+            CustomerScreen(
+                modifier = modifier, uiState = uiState, searchQuery = searchQuery,
+                onQueryChanged = viewModel::onSearchQueryChanged,
+                searchResults = searchResults
+            )
         }
 
         composable<DetailScreen.CreateCustomer> {
@@ -44,7 +53,10 @@ fun AppNavigation(
             LaunchedEffect(Unit) {
                 viewModel.uiEvent.collect { event ->
                     when (event) {
-                        BaseUiEvent.PopBackStack -> { navController.popBackStack() }
+                        BaseUiEvent.PopBackStack -> {
+                            navController.popBackStack()
+                        }
+
                         is BaseUiEvent.ShowToast -> {
                             val message = context.getString(event.message)
                             Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
