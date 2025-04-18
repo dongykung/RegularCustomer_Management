ceebbca4f684cab21cd287697ba8600baa3deb1d
kim dong kyung <kdkd422@naver.com>
2025-04-18 14:53:08 +0900

    Feat - UiEvent Message Type 변경 String -> Int(R.string.name)

--- RegularCustomerManagement/app/src/main/java/com/dkproject/regularcustomermanagement/presentation/model/BaseUiEvent.kt
+++ RegularCustomerManagement/app/src/main/java/com/dkproject/regularcustomermanagement/presentation/model/BaseUiEvent.kt
@@ -1,6 +1,8 @@
 package com.dkproject.regularcustomermanagement.presentation.model
 
+import androidx.annotation.StringRes
+
 sealed class BaseUiEvent {
-    data class ShowToast(val message: String) : BaseUiEvent()
+    data class ShowToast(@StringRes val message: Int) : BaseUiEvent()
     data object PopBackStack : BaseUiEvent()
 }
