30bf47bdb84f66a1b66ea30988b5f497560e8819
kim dong kyung <kdkd422@naver.com>
2025-04-18 09:19:26 +0900

    Feat - 고객생성 composable 뷰모델 연결

--- RegularCustomerManagement/app/src/main/java/com/dkproject/regularcustomermanagement/presentation/navigation/AppNavigation.kt
+++ RegularCustomerManagement/app/src/main/java/com/dkproject/regularcustomermanagement/presentation/navigation/AppNavigation.kt
@@ -4,18 +4,24 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
 import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.hilt.navigation.compose.hiltViewModel
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
+import com.dkproject.regularcustomermanagement.presentation.ui.createCustomer.CreateCustomerScreen
+import com.dkproject.regularcustomermanagement.presentation.ui.createCustomer.CreateCustomerViewModel
+import com.dkproject.regularcustomermanagement.presentation.ui.customer.CustomerScreen
 
 @Composable
 fun AppNavigation(
     navController: NavHostController = rememberNavController(),
     modifier: Modifier = Modifier,
 ) {
-    NavHost(navController = navController, startDestination = TabScreen.Home.route) {
+    NavHost(navController = navController, startDestination = DetailScreen.CreateCustomer) {
         composable(TabScreen.Home.route) {
             Column(modifier = modifier) {
                 Text("this is home")
@@ -23,7 +29,20 @@ fun AppNavigation(
         }
 
         composable(TabScreen.Customer.route) {
-            Text("this is customer")
+            CustomerScreen(modifier = modifier)
+        }
+
+        composable<DetailScreen.CreateCustomer> {
+            val viewModel: CreateCustomerViewModel = hiltViewModel()
+            val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+            CreateCustomerScreen(
+                uiState = uiState, onMoveStep = viewModel::updateCurrentStep,
+                popBackStack = { navController.popBackStack() },
+                updateBasicInfo = viewModel::updateBasicInfo,
+                updateAffiliationAndTags = viewModel::updateAffiliationAndTags,
+                updateStarAndMemo = viewModel::updateStarAndMemo,
+                onCompleted = viewModel::addCustomer
+            )
         }
 
         composable(TabScreen.Handover.route) {
