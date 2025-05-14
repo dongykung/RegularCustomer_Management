1df8796101397995ae4fbd0c43add33aa0737594
kim dong kyung <kdkd422@naver.com>
2025-05-14 21:58:04 +0900

    Refactor - appState remember선언

--- RegularCustomerManagement/app/src/main/java/com/dkproject/regularcustomermanagement/MainActivity.kt
+++ RegularCustomerManagement/app/src/main/java/com/dkproject/regularcustomermanagement/MainActivity.kt
@@ -4,14 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.windowsizeclass.ExperimentalMaterial3WindowSizeClassApi
-import androidx.compose.material3.windowsizeclass.calculateWindowSizeClass
-import androidx.compose.ui.Modifier
-import androidx.navigation.NavHostController
-import androidx.navigation.compose.rememberNavController
-import com.dkproject.regularcustomermanagement.presentation.Component.AdaptiveScaffold
-import com.dkproject.regularcustomermanagement.presentation.navigation.AppNavigation
+import com.dkproject.regularcustomermanagement.presentation.CustomerManageApp
+import com.dkproject.regularcustomermanagement.presentation.rememberDkAppState
 import com.dkproject.regularcustomermanagement.presentation.theme.RegularCustomerManagementTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,13 +17,9 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+            val appState = rememberDkAppState()
             RegularCustomerManagementTheme {
-                val navController: NavHostController = rememberNavController()
-                val windowSize = calculateWindowSizeClass(this)
-
-                AdaptiveScaffold(navController = navController, windowSize = windowSize.widthSizeClass) { innerPadding ->
-                    AppNavigation(navController, modifier = Modifier.padding(innerPadding))
-                }
+                CustomerManageApp(appState)
             }
         }
     }
