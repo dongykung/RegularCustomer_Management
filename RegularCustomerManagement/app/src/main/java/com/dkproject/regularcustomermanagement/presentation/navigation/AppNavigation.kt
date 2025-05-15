7b751a6d90e814cfda13a0858ae3bf54eb94fb6f
kim dong kyung <kdkd422@naver.com>
2025-05-15 23:23:31 +0900

    Refactor - Navigation수정

--- RegularCustomerManagement/app/src/main/java/com/dkproject/regularcustomermanagement/presentation/navigation/AppNavigation.kt
+++ RegularCustomerManagement/app/src/main/java/com/dkproject/regularcustomermanagement/presentation/navigation/AppNavigation.kt
@@ -1,82 +1,35 @@
 package com.dkproject.regularcustomermanagement.presentation.navigation
 
-import android.widget.Toast
-import androidx.compose.foundation.layout.Column
 import androidx.compose.material3.Text
-import androidx.compose.material3.windowsizeclass.WindowWidthSizeClass
+import androidx.compose.material3.adaptive.WindowAdaptiveInfo
+import androidx.compose.material3.adaptive.currentWindowAdaptiveInfo
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.LaunchedEffect
-import androidx.compose.runtime.getValue
+import androidx.compose.runtime.internal.composableLambda
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalContext
-import androidx.compose.ui.res.stringResource
-import androidx.hilt.navigation.compose.hiltViewModel
-import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
-import androidx.navigation.compose.rememberNavController
-import com.dkproject.regularcustomermanagement.presentation.model.BaseUiEvent
-import com.dkproject.regularcustomermanagement.presentation.ui.createCustomer.CreateCustomerScreen
-import com.dkproject.regularcustomermanagement.presentation.ui.createCustomer.CreateCustomerViewModel
-import com.dkproject.regularcustomermanagement.presentation.ui.customer.CustomerScreen
-import com.dkproject.regularcustomermanagement.presentation.ui.customer.CustomerViewModel
-import kotlinx.collections.immutable.toImmutableList
+import com.dkproject.regularcustomermanagement.presentation.DkAppState
+import com.dkproject.regularcustomermanagement.presentation.ui.customer.Customer2PaneViewModel
+import com.dkproject.regularcustomermanagement.presentation.ui.customer.CustomerListDetailScreen
 
 @Composable
 fun AppNavigation(
-    navController: NavHostController = rememberNavController(),
+    appState: DkAppState,
     modifier: Modifier = Modifier,
+    windowAdaptiveInfo: WindowAdaptiveInfo = currentWindowAdaptiveInfo(),
 ) {
-    NavHost(navController = navController, startDestination = TabScreen.Home.route) {
-        composable(TabScreen.Home.route) {
-            Column(modifier = modifier) {
-                Text("this is home")
-            }
+    val navController = appState.navController
+    NavHost(modifier = modifier, navController = navController, startDestination = Home) {
+        composable<Home> {
+            Text("This is home")
         }
 
-        composable(TabScreen.Customer.route) {
-            val viewModel: CustomerViewModel = hiltViewModel()
-            val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-            val searchQuery by viewModel.searchQuery.collectAsStateWithLifecycle()
-            val searchResults by viewModel.searchResults.collectAsStateWithLifecycle()
-            CustomerScreen(
-                modifier = modifier, uiState = uiState, searchQuery = searchQuery,
-                onQueryChanged = viewModel::onSearchQueryChanged,
-                searchResults = searchResults.toImmutableList()
-            )
+        composable<Customer> {
+            CustomerListDetailScreen()
         }
 
-        composable<DetailScreen.CreateCustomer> {
-            val context = LocalContext.current
-            val viewModel: CreateCustomerViewModel = hiltViewModel()
-            val uiState by viewModel.uiState.collectAsStateWithLifecycle()
-            LaunchedEffect(Unit) {
-                viewModel.uiEvent.collect { event ->
-                    when (event) {
-                        BaseUiEvent.PopBackStack -> {
-                            navController.popBackStack()
-                        }
-
-                        is BaseUiEvent.ShowToast -> {
-                            val message = context.getString(event.message)
-                            Toast.makeText(context, message, Toast.LENGTH_SHORT).show()
-                        }
-                    }
-                }
-            }
-            CreateCustomerScreen(
-                uiState = uiState, onMoveStep = viewModel::updateCurrentStep,
-                popBackStack = { navController.popBackStack() },
-                updateBasicInfo = viewModel::updateBasicInfo,
-                updateAffiliationAndTags = viewModel::updateAffiliationAndTags,
-                updateStarAndMemo = viewModel::updateStarAndMemo,
-                onCompleted = viewModel::addCustomer
-            )
-        }
-
-        composable(TabScreen.Handover.route) {
-            Text(text = "this is handover")
+        composable<Handover> {
+            Text("This is Handover")
         }
     }
 }
